bd36e8c855b0bc2ece21e9b083765bbbac74b46b
John Santanna <john.santanna@outlook.com>
2022-02-16 00:58:48 -0300

    Teste01Adicionar arquivos de projeto.

--- ValidacaoWorkAreas/Form1.cs
+++ ValidacaoWorkAreas/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClosedXML.Excel;
-using ValidacaoWorkAreas.Classes;
+using ValidacaoWorkAreas.Classes; 
 
 namespace ValidacaoWorkAreas
 {
